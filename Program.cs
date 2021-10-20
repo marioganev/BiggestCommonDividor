fc74e30fc46bf5a4d96cec8ef9ac6afa9919870e
User 14 <User 14@PC15>
2021-10-20 14:45:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace algoritum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+if (a =! b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+                Console.Write("Най-големият общ делител е: " + a)
+            }
+        }
+    }
+}
